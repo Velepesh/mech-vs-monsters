6afa9c2fd42f464f00b9685d7eef336a1a664ff3
Velepesh <shepelev534@gmail.com>
2022-04-14 17:50:43 +0300

    Fixed AttackHelicopter

--- Assets/Scripts/Enemy/AttackHelicopter.cs
+++ Assets/Scripts/Enemy/AttackHelicopter.cs
@@ -7,30 +7,37 @@ public class AttackHelicopter : Vehicle, IDamageable, IMover, ITarget
     [SerializeField] private float _sidewaySpeed;
     [SerializeField] private float _moveOffsetX;
 
-    private float _startPositionX;
+    private float _startPosition;
     private Vector3 _targetPosition;
 
     private void OnValidate()
     {
         _distanceToPlayer = Mathf.Clamp(_distanceToPlayer, 0f, float.MaxValue);
         _speed = Mathf.Clamp(_speed, 0f, float.MaxValue);
+        _sidewaySpeed = Mathf.Clamp(_sidewaySpeed, 0f, float.MaxValue);
+        _moveOffsetX = Mathf.Clamp(_moveOffsetX, 0f, float.MaxValue);
     }
+
+    private void Start()
+    {
+        _startPosition = transform.position.x;
+    }
+
     private void Update()
     {
         if (Target != null && Target.IsDied == false)
-        {
             Move();
-            LookAtTarget(Target.Position);
-        }
+        else
+            MoveInSpace();
     }
     public void Move()
     {
-        _targetPosition = new Vector3(transform.position.x, transform.position.y, Target.Position.z + _distanceToPlayer);
-        
-        if (transform.position.z < _targetPosition.z)
-            MoveSideway();
-        else
-            StopSideway();
+        LookAtTarget(Target.Position);
+
+        float leftX = _startPosition - _moveOffsetX + Target.Position.x;
+        float rightX = _startPosition + _moveOffsetX + Target.Position.x;
+
+        _targetPosition = new Vector3(GetSidewayX(leftX, rightX), transform.position.y, Target.Position.z + _distanceToPlayer);
 
         transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
     }
@@ -43,13 +50,17 @@ public class AttackHelicopter : Vehicle, IDamageable, IMover, ITarget
 
         transform.rotation = lookAt;
     }
-    private void MoveSideway()
-    {
-        _targetPosition = new Vector3(Mathf.Lerp(_startPositionX, _startPositionX + _moveOffsetX, (Mathf.Sin(_sidewaySpeed * Time.time) + 1.0f) / 2.0f), _targetPosition.y, _targetPosition.z);
-    }
 
-    private void StopSideway()
+    private void MoveInSpace()
     {
-        _startPositionX = transform.position.x;
+        float leftX = _startPosition - _moveOffsetX;
+        float rightX = _startPosition + _moveOffsetX;
+
+        _targetPosition = new Vector3(GetSidewayX(leftX, rightX), transform.position.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
+    }
+    private float GetSidewayX(float leftX, float rightX)
+    {
+        return Mathf.Lerp(leftX, rightX, (Mathf.Sin(_sidewaySpeed * Time.time) + 1.0f) / 2.0f);
     }
 }
